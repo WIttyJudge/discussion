7622d4f2ea98a609fc36b8da5ae5c2f443fe9e6d
Ruslan <kros8925@gmail.com>
2020-10-20 20:43:49 +0300

    ADd routing for posts, replies, tags, admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
 
+  # devise
   devise_for :users, controllers: {
-    registrations: "registrations"
+    registrations: 'registrations'
   }
 
-  resources :posts
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  resources :posts, param: :slug do
+    resource :replies, only: %i[ new create ]
+  end
+
+  resources :tags, param: :slug, only: %i[ show new create ]
+
+  namespace :admin do
+    resources :posts
+  end
+
+  get 'signout_confirm', to: 'users#signout_confirm'
 
   root 'home#index'
 end
