b473bb3ce9694104fa1f8cda1ceaa678fd944f85
Ruslan <kros8925@gmail.com>
2020-10-26 18:42:02 +0300

    Refactor ability moder of cancancan gem.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,13 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
+    can :destroy, Post if user.has_cached_role?(:moderator, Post)
+
+    can [:destroy, :update], Post, :user_id => user.id
+
     # Almighty can do anything
     if user.has_cached_role? :almighty
       can :manage, :all
-    else
-      can :destroy, Post if user.has_cached_role?(:moderator, Post)
-
-      can [:destroy, :update], Post, :user_id => user.id
     end
   end
 end
