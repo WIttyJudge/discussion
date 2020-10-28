75659925def336a0ec1dfad0298ef54c9ff186ca
Ruslan <kros8925@gmail.com>
2020-10-28 20:28:23 +0300

    Change posts ability.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
 
     can :destroy, Post if user.has_cached_role?(:moderator, Post)
 
-    can [:destroy, :update], Post, :user_id => user.id
+    can [:destroy, :update], Post, :author_id => user.id
 
     # Almighty can do anything
     if user.has_cached_role? :almighty
