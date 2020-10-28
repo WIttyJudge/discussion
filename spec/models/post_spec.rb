136d2f700de895ec5b36134a646497370053cfd2
Ruslan <kros8925@gmail.com>
2020-10-28 20:19:37 +0300

    Testing for models.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
   describe "associations" do
-    it { should belong_to(:user) }
+    it { should belong_to(:author).class_name('User') }
     it { should have_many(:replies).dependent(:destroy) }
     it { should have_and_belong_to_many(:tags) }
   end
 
   describe "validations" do
     it { should validate_presence_of(:title) }
-    it { should validate_length_of(:title) }
+    it { should validate_length_of(:title)
+        .is_at_least(10)
+        .is_at_most(128)
+    }
     it { should validate_presence_of(:body) }
   end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   describe "associations" do
-    it { should have_many(:posts) }
+    it { should have_many(:posts).with_foreign_key('author_id') }
     it { should have_many(:replies) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:username) }
+  end
+
 end
