787eaf5972d179b1897b06d03fdb7af48c1282b9
Ruslan <kros8925@gmail.com>
2020-10-28 20:28:53 +0300

    Add validation, change foreign_key user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
-  has_many :posts
+  has_many :posts, foreign_key: 'author_id'
   has_many :replies
+
+  validates :username, presence: true
 end
