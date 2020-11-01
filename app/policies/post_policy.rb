eb54f0b73d0c34ab72d5c64e5c8485e2cb577bfa
Ruslan <kros8925@gmail.com>
2020-11-01 21:49:09 +0300

    Add PostPolicy

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,23 @@
 class PostPolicy < ApplicationPolicy
+  def new?
+    user.present?
+  end
+
+  def update?
+    user_is_author? || user_is_admin?
+  end
+
   def destroy?
-    user.id == record.author_id or user.has_role? :almighty
+    user_is_author? || user_is_admin?
+  end
+
+  private
+
+  def user_is_author?
+    record.author_id == user&.id
+  end
+
+  def user_is_admin?
+    user&.has_cached_role? :almighty
   end
 end
