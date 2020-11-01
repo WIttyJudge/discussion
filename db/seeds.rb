10565e5bee9b7e276daa6c304064c68a74b0e8f0
Ruslan <kros8925@gmail.com>
2020-11-01 21:46:12 +0300

    Add seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create(
+    username: 'admin',
+    email: 'admin@gmail.com',
+    password: '123123'
+)
+
+admin.add_role :almighty
+
+3.times do
+  User.create(
+    username: Faker::Games::Dota.hero,
+    email: "#{Faker::Games::Dota.hero}@gmail.com",
+    password: '123456'
+  )
+end
+
+50.times do
+  Post.create(
+    title: Faker::Movie.title,
+    author_id: User.ids.sample,
+    body: Faker::Markdown.emphasis
+  )
+end
+
+tags = Tag.create([
+  { name: 'database' },
+  { name: 'postgresql' },
+])
