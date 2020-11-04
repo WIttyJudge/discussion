5ffd7133ce6bdd898e741f7d9e925c5dea304503
Ruslan <kros8925@gmail.com>
2020-11-04 21:59:51 +0300

    Add trait to users factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,17 @@ FactoryBot.define do
     username { generate(:username) }
     email { Faker::Internet.email }
     password { SecureRandom.hex }
+
+    # Add role to user
+    # create(:user, :with_role, role: 'myrole')
+    trait :with_role do
+      transient do
+        role { 'almighty' }
+      end
+
+      after(:create) do |user, evaluator|
+        user.add_role evaluator.role
+      end
+    end
   end
 end
