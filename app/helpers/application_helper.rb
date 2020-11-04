7574cc3416f9162bb6119a52e7b5d67c96521bcf
Ruslan <kros8925@gmail.com>
2020-11-04 21:54:49 +0300

    Add markdown method to helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+require 'redcarpet'
+
 module ApplicationHelper
+  def markdown(text)
+    options = {
+      autolink: true,
+      hard_wrap: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_spacing: true
+    }
+
+    mark = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+    mark.render(text).html_safe
+  end
 end
