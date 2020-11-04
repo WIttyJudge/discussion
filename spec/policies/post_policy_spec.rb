7c0faf5927ddef08ef18e6fe7c1f2e62dba4a552
Ruslan <kros8925@gmail.com>
2020-11-04 21:50:48 +0300

    Add policies tests

---
+++ spec/policies/post_policy_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe PostPolicy do
+  subject { described_class.new(user, post) }
+
+  let(:post) { create(:post) }
+
+  describe "visitor" do
+    let(:user) { nil }
+
+    it { should forbid_actions(%i[new create edit update destroy]) }
+  end
+
+  describe "signed in user" do
+    let(:user) { create(:user) }
+
+    it { should permit_actions(%i[new create]) }
+  end
+
+  describe ".user_author?" do
+    let(:user) { create(:user) }
+    let(:post) { create(:post, author: user) }
+
+    it { should permit_actions(%i[new create edit update destroy]) }
+  end
+
+  describe ".user_almighty?" do
+    let(:user) { create(:user, :with_role) }
+
+    it { should permit_actions(%i[new create edit update destroy]) }
+  end
+end
