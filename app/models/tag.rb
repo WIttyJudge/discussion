5b55ec3e7fb3bfa60b936efd97c6b9b341fff26a
Ruslan <kros8925@gmail.com>
2020-11-07 17:08:29 +0300

    Add decription and guideline record to tags model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :posts
 
-  validates :name, presence: true
+  validates :name, :guideline, :about, presence: true
   # validates :name, uniqueness: { case_sensitive: false }
 
   before_save :name_capitalize, :create_slug
