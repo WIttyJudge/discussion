31ed2fb5a2835aa98f16ac6307c9e611373b1d6a
Ruslan <kros8925@gmail.com>
2020-11-08 21:53:22 +0300

    Add validates to reply field.

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,6 @@
 class Reply < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :reply, presence: true
 end
--- spec/models/reply_spec.rb
+++ spec/models/reply_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Reply, :type => :model do
     it { should belong_to(:post) }
     it { should belong_to(:user) }
   end
+
+  describe "validates" do
+    it { should validate_presence_of(:reply) }
+  end
 end
