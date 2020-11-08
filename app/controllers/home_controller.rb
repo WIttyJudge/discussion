840f64e3202d60a359b98aa049c564b7b4b5b997
Ruslan <kros8925@gmail.com>
2020-11-08 21:48:29 +0300

    Add :replies to include in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.includes(:author, :tags).recent
+    @posts = Post.includes(:author, :tags, :replies).recent
     @tags = Tag.alphabetical
   end
 end
