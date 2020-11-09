60a3c4e21adcbe28132e997fbc93a02fdfaddddd
Ruslan <kros8925@gmail.com>
2020-11-09 20:09:57 +0300

    Change code in migration to improve performance

--- db/migrate/20201107114103_add_guideline_to_tags.rb
+++ db/migrate/20201107114103_add_guideline_to_tags.rb
@@ -1,6 +1,7 @@
 class AddGuidelineToTags < ActiveRecord::Migration[6.0]
   def change
-    add_column :tags, :guideline, :string, null: false, default: ''
+    add_column :tags, :guideline, :string, null: false
+    change_column_default :tags, :guideline, ''
     change_column_default :tags, :guideline, nil
   end
 end
