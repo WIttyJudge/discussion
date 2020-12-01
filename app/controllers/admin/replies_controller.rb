e41d54b8882e7df6472324550a73c40897ee108d
Ruslan <kros8925@gmail.com>
2020-12-01 14:55:55 +0300

    Add delete method

--- app/controllers/admin/replies_controller.rb
+++ app/controllers/admin/replies_controller.rb
@@ -1,7 +1,23 @@
 module Admin
   class RepliesController < BaseController
+    before_action :set_reply, only: %i[destroy]
+
     def index
       @replies = Reply.includes(:user)
+    end
+
+    def destroy
+      @reply.destroy
+
+      text = 'Reply was successfully deleted.'
+      flash[:notice] = text
+      redirect_to admin_replies_path
+    end
+
+    private
+
+    def set_reply
+      @reply = Reply.find(params[:id])
     end
   end
 end
