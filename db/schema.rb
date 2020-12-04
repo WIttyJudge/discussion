af7719db4783ffd9c3122da1f99ca3a0ef28e0a5
Ruslan <kros8925@gmail.com>
2020-12-04 23:47:23 +0300

    Schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_131006) do
+ActiveRecord::Schema.define(version: 2020_12_03_211850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_131006) do
     t.string "username", null: false
     t.string "summery"
     t.string "location"
+    t.string "name", null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
