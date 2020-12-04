9765f77d4ff455f20cb1ad6f2adf6b6c2d670f22
Ruslan <kros8925@gmail.com>
2020-12-04 23:48:52 +0300

    Add validation to username and user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :replies
 
+  validates :name, presence: true
   validates :username, presence: true
+  validates :username, format: { without: /\s/ }
   validates :summery, length: { maximum: 200 }
 end
