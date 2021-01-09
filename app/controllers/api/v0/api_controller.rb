7f532421062cad568cfbfd221741ca5e4dfa0f36
Ruslan <kros8925@gmail.com>
2021-01-09 14:00:34 +0300

    Handle ParameterMissing exeption.

--- app/controllers/api/v0/api_controller.rb
+++ app/controllers/api/v0/api_controller.rb
@@ -5,14 +5,20 @@ module Api
 
       rescue_from ActiveRecord::RecordNotFound, with: :error_not_found
 
+      rescue_from ActionController::ParameterMissing do
+        error_unprocessable_entity('Parameters missing')
+      end
+
       respond_to :json
+
+      protected
 
       def error_unprocessable_entity(message)
         render json: { errors: { message: message } }, status: :unprocessable_entity
       end
 
       def error_not_found
-        render json: { errors: { message: 'not found' } }, status: :not_found
+        render json: { errors: { message: 'Not found' } }, status: :not_found
       end
     end
   end
